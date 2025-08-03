9db8ea5242c120ec1a69586bcc2b161ef6ef058d
Sara <kr.sara.prv@gmail.com>
2025-08-03 18:05:24 +0200

    button idea changed

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 
-const clickSound = new Audio("mouse-button-fast-click.wav"); // Put the sound file in the same folder
-const blob = document.querySelector('.blob');
-const button = document.querySelector('button');
-
-function playClick() {
-  clickSound.currentTime = 0;
-  clickSound.play();
+function morphdrop(){
+  const btn=document.querySelector("button");
+  btn.classList.add('morphing');
+  btn.onclick = null;
+  btn.innerText = " ";
+  setTimeout(() =>{
+    btn.remove();
+    createspalsh();
+  }, 1100);
 }
 
-button.addEventListener("click", () => {
-  // Get button center at click time
-  const rect = button.getBoundingClientRect();
-  const centerX = rect.left + rect.width / 2;
-  const centerY = rect.top + rect.height / 2;
-  blob.style.left = `${centerX - 50}px`;
-  blob.style.top = `${centerY - 50}px`;
-  blob.offsetWidth;
-  blob.style.transform = "scale(30)";
-  playClick();
-});
+function createspalsh (){
+  const splash = document.createElement("div");
+  splash.className = "splash";
+  document.body.appendChild(splash);
+
+  setTimeout(() => {
+    splash.remove();
+  }, 600);
+
+}
+
+
+
+
+
