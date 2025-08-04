2e06c54ff7963a82c056d0028a89e81b0c171157
Sara <kr.sara.prv@gmail.com>
2025-08-04 21:00:06 +0200

    changed the whole idea + missed a commit

--- script.js
+++ script.js
@@ -1,26 +1,4 @@
 
-function morphdrop(){
-  const btn=document.querySelector("button");
-  btn.classList.add('morphing');
-  btn.onclick = null;
-  btn.innerText = " ";
-  setTimeout(() =>{
-    btn.remove();
-    createspalsh();
-  }, 1100);
-}
-
-function createspalsh (){
-  const splash = document.createElement("div");
-  splash.className = "splash";
-  document.body.appendChild(splash);
-
-  setTimeout(() => {
-    splash.remove();
-  }, 600);
-
-}
-
 
 
 
